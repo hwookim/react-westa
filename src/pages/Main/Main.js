35b176360260a1b7e051541d448a0d3568a48d52
hwookim <gusdn9754@gmail.com>
2022-11-10 01:15:04 +0900

    feat: 삭제 기능 구현

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -15,10 +15,21 @@ const Main = () => {
         setTodo("");
     }
 
+    const handleClickDelete = (index) => () => {
+        const deleted = [...todos];
+        deleted.splice(index, 1);
+        setTodos(deleted);
+    }
+
     return (
         <div>
             <input value={todo} onChange={handleChange} onKeyUp={handleKeyDown} />
-            {todos.map((value, index) => <div key={index}>{value}</div>)}
+            {todos.map((value, index) =>
+                <div key={index}>
+                    {value}
+                    <button onClick={handleClickDelete(index)}>x</button>
+                </div>
+            )}
         </div>
     )
 }
